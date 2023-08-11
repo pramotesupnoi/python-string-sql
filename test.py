212c13390b1220fc6eb409158cd7f5189fe287b7
Pramote Supnoi <pramotesupnoi@gmail.com>
2023-08-11 12:40:14 +0700

    update capture position

--- test.py
+++ test.py
@@ -2,11 +2,16 @@ def hello_world():
     my_value = 1
     sql = f""" select * from tb_greeting """
     sql = f""" insert into tb_greeting (index) value (1) """
-    sql = "  select * FROM me"
+    sql = " select * FROM me"
     sql = f" select * FROM me "
-    sql = f"""      insert into tb_greeting (index) value ({my_value}) 
+    sql = f"select * FROM me "
+    sql = "select * FROM me "
+    sql = f""" insert into tb_greeting (index) value ({my_value}) 
     """
     sql = f"""
+    insert into tb_greeting (index) value (1) 
+    """
+    sql = """
     insert into tb_greeting (index) value (1) 
     """
     
