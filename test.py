5227614e786b0e83071958a66f92a616f9e2d6eb
Pramote Supnoi <pramotesupnoi@gmail.com>
2023-08-11 11:45:58 +0700

    update pattern

---
+++ test.py
@@ -0,0 +1,13 @@
+def hello_world():
+    my_value = 1
+    sql = f""" select * from tb_greeting """
+    sql = f""" insert into tb_greeting (index) value (1) """
+    sql = "  select * FROM me"
+    sql = f" select * FROM me "
+    sql = f"""      insert into tb_greeting (index) value ({my_value}) 
+    """
+    sql = f"""
+    insert into tb_greeting (index) value (1) 
+    """
+    
+    return "ok"
